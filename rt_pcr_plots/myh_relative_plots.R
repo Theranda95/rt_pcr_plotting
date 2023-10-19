5b2d8f23a7fde7deec5017b5c3deba33088dcb0f
Theranda Jashari <therandajashari@Therandas-MBP.lan>
2023-10-18 21:46:16 -0400

    qpcr plot bar for black background

--- rt_pcr_plots/myh_relative_plots.R
+++ rt_pcr_plots/myh_relative_plots.R
@@ -25,7 +25,9 @@ myh_rel$Genotype <- factor(myh_rel$Genotype, levels = c("WT25", "WT26", "KO1", "
 #Use geom_col() instead of geom_bar() for heights of the bar representing values in the data. 
 #geom_bar() more often used for histograms 
 
-rel_plot <- ggplot(myh_rel, aes (x = Genotype, y = FC)) +
+##---------------plotting for black background ---------------------
+
+rel_plot <- ggplot(myh_rel, aes (x = Genotype, y = FC, fill = Genotype)) +
   #indicate what plot you want and define characteristics 
   geom_bar(position = "dodge",
            stat = "summary",
@@ -37,21 +39,29 @@ rel_plot <- ggplot(myh_rel, aes (x = Genotype, y = FC)) +
   #Add meant point and error bars using stat summary
    stat_summary(fun.y = mean,
                geom = "point",
-               size = 2) +
+               size = 2,
+               color = "grey") +
   stat_summary(fun.data = mean_sd,
                geom = "errorbar",
-               width = 0.25) +
-  #change axis titles and themes 
+               width = 0.25,
+               color = "grey") +
+  #change axis titles and themes
   theme_minimal() +
+  theme(panel.grid = element_line(color = "#555555")) + #changing the color of the grid to fit better in a black background 
   labs(x = NULL, y = "Relative to RPS18 expression") +
   scale_x_discrete(labels = custom_labels) +
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1, size = 10, color = "#F0F0F0")) +
-  theme(axis.title.y = element_text(size = 13, color = "#F0F0F0")) + 
-  theme(axis.text.y = element_text(color = "#F0F0F0")) +
-  theme(strip.text = element_text(size = 14, color = "#F0F0F0"))
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1, size = 10, color = "#F0F0F0"),
+        axis.title.y = element_text(size = 13, color = "#F0F0F0"),
+        axis.text.y = element_text(color = "#F0F0F0"),
+        strip.text = element_text(size = 14, color = "#F0F0F0"),
+        legend.position = "none") +
+  scale_fill_manual(values = c("#7FB3D5","#7FB3D5","#0059b3","#0059b3")) 
+  
   
 ## Save the graph 
-ggsave(filename = "myh_relative_exp.png", plot = rel_plot, device = "png", bg = "transparent", width = 4.51, height = 3.98)
+setwd("/Users/therandajashari/Documents/experiments_2023/12_sept_23_RNA_Seq_Analysis/qPCR_validations/rt_pcr_plotting")
+getwd()
+ggsave(filename = "myh_relative_exp.png", plot = rel_plot, device = "png", bg = "transparent", width = 4.69, height = 3.59)
 
 ##------------------------ Notes --------------------------------   
     ##I am dividing the data by facets here but it can also be divided into a grouped
